62bb987b13d9bb5a12214ed8f7deb4a97ac2fd15
Artur Roszczyk <artur.roszczyk@gmail.com>
2013-04-26 18:53:51 +0200

    Basic authentication for admin/plugins controller

--- app/controllers/admin/plugins_controller.rb
+++ app/controllers/admin/plugins_controller.rb
@@ -1,4 +1,8 @@
 class Admin::PluginsController < ApplicationController
+  if ENV['ADMIN_PASSWORD']
+    http_basic_authenticate_with name: "admin", password: ENV["ADMIN_PASSWORD"]
+  end
+
   def index
     @plugins = Plugin.waiting.map { |plugin| PluginPresenter.new(plugin, view_context) }
   end
