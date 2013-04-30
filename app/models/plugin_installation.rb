e249c68854f15dee5f4c079ce2972e566dc50cbe
Artur Roszczyk <artur.roszczyk@gmail.com>
2013-04-30 21:14:21 +0200

    Handle uniqueness of plugin installations differently

--- app/models/plugin_installation.rb
+++ app/models/plugin_installation.rb
@@ -1,4 +1,5 @@
 class PluginInstallation < ActiveRecord::Base
   belongs_to :user
   belongs_to :plugin
+  validates :plugin_id, uniqueness: {scope: :user_id}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :plugin_installations
-  has_many :plugins, :through => :plugin_installations, uniq: true
+  has_many :plugins, :through => :plugin_installations
 
   scope :by_auth, ->(provider, uid) { where(provider: provider, uid: uid) }
 
